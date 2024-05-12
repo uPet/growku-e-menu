a5f9f3e5e916bcb3971c9801e98420e9dc3b4261
MohammedAlghazali <mohammed.m.alghazali@hotmail.com>
2024-05-12 03:31:15 +0200

    Render the product details in a modal

--- src/components/organisms/ProductCard/ProductCard.tsx
+++ src/components/organisms/ProductCard/ProductCard.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useRef } from "react";
 
 import "./productCard.css";
 
@@ -7,35 +7,41 @@ import CardActionArea from "../../atoms/CardActionArea/CardActionArea";
 import CardMedia from "../../atoms/CardMedia/CardMedia";
 import CardContent from "../../atoms/CardContent/CardContent";
 import CloseIcon from "../../atoms/CloseIcon";
-import { useState } from "react";
 
 interface Product {
   title: string;
   description: string;
   price: string;
-  media: { type: 'image' | 'video'; url: string }[];
+  media: { type: "image" | "video"; url: string }[];
 }
 interface ProductCardProps {
   product: Product;
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
-  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  // const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const productModalRef = useRef<HTMLDivElement>(null);
 
-  const openModal = (product: Product) => {
-    setSelectedProduct(product);
+  const openModal = () => {
+    // setSelectedProduct(product);
     document.body.style.overflow = "hidden"; // Disable scrolling when modal is open
+    if (productModalRef.current) {
+      productModalRef.current.style.visibility = "visible";
+    }
   };
 
   const closeModal = () => {
-    setSelectedProduct(null);
+    // setSelectedProduct(null);
     document.body.style.overflow = ""; // Enable scrolling when modal is closed
+    if (productModalRef.current) {
+      productModalRef.current.style.visibility = "hidden";
+    }
   };
 
   return (
     <>
       <Card>
-        <CardActionArea onClick={() => openModal(product)}>
+        <CardActionArea onClick={() => openModal()}>
           <CardContent>
             <h3 className="h5">{product.title}</h3>
             <p>{product.description}</p>
@@ -44,37 +50,35 @@ export default function ProductCard({ product }: ProductCardProps) {
           <CardMedia src={product?.media?.[0]?.url} alt={product.title} />
         </CardActionArea>
       </Card>
-      {selectedProduct && (
-        <div className="modal-overlay" onClick={closeModal}>
-          <div className="modal" onClick={(e) => e.stopPropagation()}>
-            <button className="close-button" onClick={closeModal}>
-              <CloseIcon />
-            </button>
-            <h3 className="h5">{selectedProduct.title}</h3>
-            <p>{selectedProduct.description}</p>
-            <p>{selectedProduct.price}</p>
-            {/* <img src={selectedProduct.image} alt={selectedProduct.title} /> */}
-            <div className="media-container">
-              {selectedProduct.media.map((item, index) => (
-                <React.Fragment key={index}>
-                  {item.type === "image" ? (
-                    <img
-                      src={item.url}
-                      alt={`${selectedProduct.title}-${index}`}
-                      className="modal-media"
-                    />
-                  ) : (
-                    <video controls className="modal-media">
-                      <source src={item.url} type="video/mp4" />
-                      Your browser does not support the video tag.
-                    </video>
-                  )}
-                </React.Fragment>
-              ))}
-            </div>
+      <div className="modal-overlay" ref={productModalRef} onClick={closeModal}>
+        <div className="modal" onClick={(e) => e.stopPropagation()}>
+          <button className="close-button" onClick={closeModal}>
+            <CloseIcon />
+          </button>
+          <h3 className="h5">{product.title}</h3>
+          <p>{product.description}</p>
+          <p>{product.price}</p>
+          {/* TODO: Update the layout to "Quilted image list" layout */}
+          <div className="media-container">
+            {product.media.map((item, index) => (
+              <React.Fragment key={index}>
+                {item.type === "image" ? (
+                  <img
+                    src={item.url}
+                    alt={`${product.title}-${index}`}
+                    className="modal-media"
+                  />
+                ) : (
+                  <video controls className="modal-media">
+                    <source src={item.url} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                )}
+              </React.Fragment>
+            ))}
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 }
