2dac6a18efe1fedccec334e0a45fa4d5f2ce921b
MohammedAlghazali <mohammed.m.alghazali@hotmail.com>
2024-05-15 00:34:20 +0200

    remove the images cache

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -11,10 +11,10 @@
 import CryptoJS from "crypto-js";
 import { Store, get, set } from 'idb-keyval';
 import { clientsClaim } from "workbox-core";
-import { ExpirationPlugin } from "workbox-expiration";
+// import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
+// import { StaleWhileRevalidate } from "workbox-strategies";
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -57,39 +57,39 @@ registerRoute(
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
-registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
-  ({ url }) =>
-    url.origin === self.location.origin && url.pathname.endsWith(".png"),
-  // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
-    cacheName: "images",
-    plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
-    ],
-  })
-);
+// registerRoute(
+//   // Add in any other file extensions or routing criteria as needed.
+//   ({ url }) =>
+//     url.origin === self.location.origin && url.pathname.endsWith(".png"),
+//   // Customize this strategy as needed, e.g., by changing to CacheFirst.
+//   new StaleWhileRevalidate({
+//     cacheName: "images",
+//     plugins: [
+//       // Ensure that once this runtime cache reaches a maximum size the
+//       // least-recently used images are removed.
+//       new ExpirationPlugin({ maxEntries: 50 }),
+//     ],
+//   })
+// );
 
 // Cache all images on your site
-registerRoute(
-  ({ request }) => request.destination === "image",
-  new StaleWhileRevalidate({
-    cacheName: "all-images",
-    plugins: [
-      new ExpirationPlugin({ maxEntries: 100 }), // Adjust maxEntries as needed
-    ],
-  })
-);
+// registerRoute(
+//   ({ request }) => request.destination === "image",
+//   new StaleWhileRevalidate({
+//     cacheName: "all-images",
+//     plugins: [
+//       new ExpirationPlugin({ maxEntries: 100 }), // Adjust maxEntries as needed
+//     ],
+//   })
+// );
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
+// self.addEventListener("message", (event) => {
+//   if (event.data && event.data.type === "SKIP_WAITING") {
+//     self.skipWaiting();
+//   }
+// });
 
 // Any other custom service worker logic can go here.
 
