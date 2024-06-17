8562d3e21ac4b737987f3c8a68b954003108c41a
MohammedAlghazali <mohammed.m.alghazali@hotmail.com>
2024-06-17 19:16:24 +0200

    Revert "Add the support of the categories custom order"
    This reverts commit 6553b1f42740eb723ab3eddca9eb996f0e410136.

--- src/api/categories-graphql.ts
+++ src/api/categories-graphql.ts
@@ -12,16 +12,6 @@ query collections($first: Int, $language: LanguageCode!) @inContext(language: $l
       node {
         id
         title
-        metafields(identifiers: [
-          {namespace: "custom", key: "collection_order"}
-        ]) {
-          id
-          key
-          namespace
-          description
-          type
-          value
-        }
         products(first: 100) {
           edges {
             node {
@@ -161,22 +151,6 @@ const shopifyCollectionToCategory = (
   };
 };
 
-const sortCollections = (
-  collections: ShopifyCollectionQuery[]
-): ShopifyCollectionQuery[] => {
-  return collections.sort((a: ShopifyCollectionQuery, b: ShopifyCollectionQuery) => {
-    const orderA =
-      a.node.metafields?.[0]?.key === "collection_order"
-        ? parseInt(a.node.metafields?.[0].value, 10)
-        : Number.MAX_SAFE_INTEGER;
-    const orderB =
-      b.node.metafields?.[0]?.key === "collection_order"
-        ? parseInt(b.node.metafields?.[0].value, 10)
-        : Number.MAX_SAFE_INTEGER;
-    return orderA - orderB;
-  });
-};
-
 export const getCategoriesData = async (
   language: LanguageType = "EN"
 ): Promise<Category[] | undefined> => {
@@ -205,9 +179,7 @@ export const getCategoriesData = async (
     return;
   }
 
-  const sortedCollections = sortCollections(data.data.collections.edges);
-
-  return sortedCollections
+  return data.data.collections.edges
     .filter(
       (edge: ShopifyCollectionQuery) => edge.node.products.edges.length > 0
     )
