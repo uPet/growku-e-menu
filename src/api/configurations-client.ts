9934648f776853c76c94c324aed9acbac440423d
Mohammed Alghazali <mohammedalghazali@alMacBook-Air-alkhas-bMohammed.local>
2024-12-06 13:40:26 +0200

    Add the config data as static

--- src/api/configurations-client.ts
+++ src/api/configurations-client.ts
@@ -1,7 +1,7 @@
 import { gapi } from "gapi-script";
 import { ConfigItem } from "../model/configurations";
-import { SheetsResponse } from "./model/GoogleSheets";
-import { Option } from "../model/configurations";
+// import { SheetsResponse } from "./model/GoogleSheets";
+// import { Option } from "../model/configurations";
 
 export default class ConfigurationsClient {
   private apiKey: string;
@@ -53,30 +53,42 @@ export default class ConfigurationsClient {
   }
 
   public async getConfigurationsData(): Promise<ConfigItem[]> {
-    try {
-      await this.initializeClient(); // Ensure client is initialized before making API request
-      const firstSheetName = await this.getFirstSheetName();
-      const range = `${firstSheetName}!A1:B`; // Get the data of the columns A and B only
-      const response = (await gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: this.spreadsheetId,
-        range: range,
-      })) as { result: SheetsResponse };
-
-      const result = response?.result;
-      if (result.values) {
-        return result.values.slice(1).map((row: Option[]): ConfigItem => {
-          return {
-            option: row[0],
-            value: row[1],
-          };
-        });
-      } else {
-        return [];
+    // Return static data for as we are going to change the source of data
+    return [
+      {
+        option: 'video-url',
+        value: 'https://cdn.shopify.com/videos/c/vp/176f7be3f0d74fd4bf4af1ce9d855bc8/176f7be3f0d74fd4bf4af1ce9d855bc8.HD-720p-1.6Mbps-28991975.mp4'
+      },
+      {
+        option: 'store-name',
+        value: 'Growku'
       }
-    } catch (error: any) {
-      throw new Error(
-        "Error reading Google Sheets data: " + error.message || ""
-      );
-    }
+    ];
+
+    // try {
+    //   await this.initializeClient(); // Ensure client is initialized before making API request
+    //   const firstSheetName = await this.getFirstSheetName();
+    //   const range = `${firstSheetName}!A1:B`; // Get the data of the columns A and B only
+    //   const response = (await gapi.client.sheets.spreadsheets.values.get({
+    //     spreadsheetId: this.spreadsheetId,
+    //     range: range,
+    //   })) as { result: SheetsResponse };
+
+    //   const result = response?.result;
+    //   if (result.values) {
+    //     return result.values.slice(1).map((row: Option[]): ConfigItem => {
+    //       return {
+    //         option: row[0],
+    //         value: row[1],
+    //       };
+    //     });
+    //   } else {
+    //     return [];
+    //   }
+    // } catch (error: any) {
+    //   throw new Error(
+    //     "Error reading Google Sheets data: " + error.message || ""
+    //   );
+    // }
   }
 }
