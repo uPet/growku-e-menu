d9c27ef250a340167e06c24a0fdb199b10c5350b
Mohammed Alghazali <mohammedalghazali@alMacBook-Air-alkhas-bMohammed.local>
2025-04-29 01:39:29 +0300

    Remove test changes

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -10,7 +10,15 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://cra.link/PWA
 
-const isLocalhost = false;
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
 
 type Config = {
   onSuccess?: (registration: ServiceWorkerRegistration) => void;
@@ -18,19 +26,18 @@ type Config = {
 };
 
 export function register(config?: Config) {
-  if (true && "serviceWorker" in navigator) {
+  if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    // const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-    // if (publicUrl.origin !== window.location.origin) {
-    //   // Our service worker won't work if PUBLIC_URL is on a different origin
-    //   // from what our page is served on. This might happen if a CDN is used to
-    //   // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-    //   return;
-    // }
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicUrl.origin !== window.location.origin) {
+      // Our service worker won't work if PUBLIC_URL is on a different origin
+      // from what our page is served on. This might happen if a CDN is used to
+      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      return;
+    }
 
     window.addEventListener("load", () => {
-      console.log("process.env.PUBLIC_URL :>> ", process.env.PUBLIC_URL);
-      const swUrl = `/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
